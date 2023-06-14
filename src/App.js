954f90946e04553e8080309b517ac8b8c91675a8
Sumeya <sumeyaibrahim72@gmail.com>
2023-06-14 22:24:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Categories from './pages/Categories';
+import store from './redux/store';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+      <Provider store={store}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
